38c019e9e081546faa47dc1f16304ebe3d8110bc
Benjamin Johnson <bjohnson@fcusd.org>
2024-08-12 21:46:20 -0700

    Update String1.java

--- String1.java
+++ String1.java
@@ -37,7 +37,7 @@ public class String1
      *  helloName("X") → "Hello X!"
      */
     public String helloName(String name) {
-        return unimplemented;
+        return "Hello " + name + "!";//unimplemented;
     }
 
     /*
